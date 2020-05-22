ff1266adb911201ee2ce508f48f6b24f199ad3c6
sync667 <sync667@gmail.com>
2020-05-22 12:16:53 +0200

    deel ios

--- ngx/index.js
+++ ngx/index.js
@@ -6,14 +6,14 @@ var Braintree = /** @class */ (function (_super) {
     function Braintree() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
-    Braintree.prototype.initialize = function (token) { return cordova(this, "initialize", { "platforms": ["Android", "iOS"] }, arguments); };
-    Braintree.prototype.setupApplePay = function (options) { return cordova(this, "setupApplePay", { "platforms": ["iOS"] }, arguments); };
-    Braintree.prototype.presentDropInPaymentUI = function (options) { return cordova(this, "presentDropInPaymentUI", { "platforms": ["Android", "iOS"] }, arguments); };
+    Braintree.prototype.initialize = function (token) { return cordova(this, "initialize", { "platforms": ["Android"] }, arguments); };
+    Braintree.prototype.setupApplePay = function (options) { return cordova(this, "setupApplePay", { "platforms": [] }, arguments); };
+    Braintree.prototype.presentDropInPaymentUI = function (options) { return cordova(this, "presentDropInPaymentUI", { "platforms": ["Android"] }, arguments); };
     Braintree.pluginName = "Braintree";
     Braintree.plugin = "cordova-plugin-braintree-3ds";
     Braintree.pluginRef = "BraintreePlugin";
     Braintree.repo = "https://github.com/MedITSolutionsKurman/cordova-plugin-braintree-3ds";
-    Braintree.platforms = ["Android", "iOS"];
+    Braintree.platforms = ["Android"];
     Braintree.install = "ionic cordova plugin add https://github.com/MedITSolutionsKurman/cordova-plugin-braintree-3ds";
     Braintree.installVariables = [];
     Braintree = __decorate([
--- src/ios/AppDelegate+Braintree.h
+++
@@ -1,5 +0,0 @@
-#import "AppDelegate.h"
-
-@interface AppDelegate (BraintreePlugin) <UIApplicationDelegate>
-+ (AppDelegate *) instance;
-@end
